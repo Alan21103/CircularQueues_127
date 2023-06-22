e2c824ff14ab2d5ec33d2723ce04e25e9a06ae61
Alan21103 <syahlansaputra03@gmail.com>
2023-06-22 15:14:57 +0700

    mengambil input angka dari pengguna dan memasukkannya ke dalam antrian (queue)

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -10,40 +10,9 @@ public:
 		FRONT = -1;
 		REAR = -1;
 	}
-	
+
 	void insert() {
 		int num;
 		cout << "Enter a number: ";
 		cin >> num;
 		cout << endl;
-
-		// Cek apakah antrian penuh
-		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-			cout << "\nQueue overflow\n";
-			return;
-		}
-
-		// Cek apakah antrian kosong
-		if (FRONT == -1) {
-			FRONT = 0;
-			REAR = 0;
-		}
-		else {
-			// Jika REAR berada di posisi terakhir array, kembali ke awal array
-			if (REAR == max - 1)
-				REAR = 0;
-			else
-				REAR = REAR + 1;
-		}
-		queue_array[REAR] = num;
-	}
-
-	void remove() {
-		//Cek apakah antrian kosong
-		if (FRONT == -1) {
-			cout << "Queue underflow\n";
-			return;
-		}
-		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
-	}
-};
